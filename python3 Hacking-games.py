50dcbde1302fd5cc9ec60ba63569550c9ec63979
Hacker1hassan <126151979+Hacker1hassan@users.noreply.github.com>
2023-05-23 08:52:50 +0800

    hacking-games

--- python3 Hacking-games.py
+++ python3 Hacking-games.py
@@ -1,2 +1,7 @@
-# hacking-games
-Hack any game
+apt update && apt upgrade
+pkg install python
+pkg install python2
+pkg install python3
+git clone https://github.com/Hacker1hassan/hacking-games.git
+cd hacking-games
+python3 brute.py
